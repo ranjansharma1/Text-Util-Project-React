6e33c3bd3f829991a6c08c54e32a7e4beff5eb86
ranja <ranjansharmadto@gmail.com>
2023-02-01 19:21:48 +0530

    fixing some color alignment

--- src/components/About.js
+++ src/components/About.js
@@ -3,39 +3,24 @@ import React from "react";
 export default function About(props) {
   let myStyle = {
     color: props.mode==="light"?"black":"white",
-
-    backgroundColor: props.mode==="light"?"white":"black",
+    backgroundColor: props.mode==="light"?"white":"#3d3d3d",
     border: "1px solid",
-    borderColor: props.mode==="light"?"black":"white",
-    
+    borderColor: props.mode==="light"?"black":"white",    
   };
 
 
   return (
     <div className="container my-3" >
-      <h2 style={{color:"red"}}>About Us</h2>
+      <h2 style={{color:"#ff6000"}}>About Us</h2>
       <div className="accordion" id="accordionExample">
         <div className="accordion-item">
           <h2 className="accordion-header" id="headingOne">
-            <button
-              className="accordion-button"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#collapseOne"
-              aria-expanded="true"
-              aria-controls="collapseOne"
-              style={myStyle}
-            >
+            <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" style={myStyle}>
               <strong>Restructure your Text</strong>
             </button>
           </h2>
-          <div
-            id="collapseOne"
-            className="accordion-collapse collapse show"
-            aria-labelledby="headingOne"
-            data-bs-parent="#accordionExample"
-          >
-            <div className="accordion-body" style={myStyle}>
+          <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+            <div className="accordion-body" style={{backgroundColor:'#5e5f607d'}}>
               Text Utils give you a different way to restructire your text and make it most readable.
               It will help you to make your text more readable and easy to read.
             </div>
@@ -43,51 +28,24 @@ export default function About(props) {
         </div>
         <div className="accordion-item">
           <h2 className="accordion-header" id="headingTwo">
-            <button
-              className="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#collapseTwo"
-              aria-expanded="false"
-              aria-controls="collapseTwo"
-              style={myStyle}
-            >
+            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" style={myStyle}>
               <strong>Free To Use</strong>
             </button>
           </h2>
-          <div
-            id="collapseTwo"
-            className="accordion-collapse collapse"
-            aria-labelledby="headingTwo"
-            data-bs-parent="#accordionExample"
-          >
-            <div className="accordion-body" style={myStyle}>
+          <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+            <div className="accordion-body" style={{backgroundColor:'#5e5f607d'}}>
               It is totaly Free to Use , So you can take your text and make it easy to read.
-
             </div>
           </div>
         </div>
         <div className="accordion-item">
           <h2 className="accordion-header" id="headingThree">
-            <button
-              className="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#collapseThree"
-              aria-expanded="false"
-              aria-controls="collapseThree"
-              style={myStyle}
-            >
+            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree" style={myStyle} >
              <strong>Contact Us</strong> 
             </button>
           </h2>
-          <div
-            id="collapseThree"
-            className="accordion-collapse collapse"
-            aria-labelledby="headingThree"
-            data-bs-parent="#accordionExample"
-          >
-            <div className="accordion-body" style={myStyle}>
+          <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample" >
+            <div className="accordion-body" style={{backgroundColor:'#5e5f607d'}}>
               Name: Ranjan Sharma
               <br/>
               Address: Gate 1, Near hotel Tarachandi, Madras, India-526413 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,27 +3,16 @@ import PropTypes from "prop-types"; //short form: impt
 import { Link } from "react-router-dom";
 
 export default function Navbar(props) {
+
   return (
     <>
-
-      <nav
-        className={`navbar navbar-expand-lg `}
-        style={{ backgroundColor: props.colorMode , color: "white" }}
-      >
+      <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`} >
         <div className="container-fluid ">
           <Link className="navbar-brand" to="/">
             {/* it will take value from Navabr.js  */}
             {props.title}
           </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -39,25 +28,22 @@ export default function Navbar(props) {
                 </Link>
               </li>
             </ul>
-            <form className="mx-3">
-              <input  type="radio" id="blue"  name="color" value="blue" checked={props.colorMode === 'blue'} onChange={(e) => props.colorChangeMode(e.target.value)}/>
-              <label htmlFor="blue">Blue</label>
+            <form className={`mx-3 text-${ props.mode === "light" ? "dark" : "light" }`} >
+              <input type="radio"  id="blue" name="color" value="blue" checked={props.colorMode === "blue"} onChange={(e) => props.colorChangeMode(e.target.value)}/>
+              <label className="mx-2" htmlFor="blue"> Blue </label>
 
-              <input type="radio" id="green" name="color" value="green" checked={props.colorMode === 'green'} onChange={(e) => props.colorChangeMode(e.target.value)}/>
-              <label htmlFor="green">Green</label>
+              <input type="radio" id="green" name="color" value="green" checked={props.colorMode === "green"} onChange={(e) => props.colorChangeMode(e.target.value)}/>
+              <label className="mx-2" htmlFor="green">Green</label>
 
-              <input type="radio" id="red" name="color" value="red" checked={props.colorMode === 'red'} onChange={(e) => props.colorChangeMode(e.target.value)}/>
-              <label htmlFor="red">Red</label>
+              <input type="radio" id="red" name="color" value="red" checked={props.colorMode === "red"} onChange={(e) => props.colorChangeMode(e.target.value)}/>
+              <label className="mx-2" htmlFor="red">Red</label>
             </form>
 
             {/* htmlFor Enabling Dark Mode */}
             <div className={`form-check form-switch text-${ props.mode === "light" ? "dark" : "light" }`} >
               <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="dId"/>
-              <label className="form-check-label" htmlFor="flexSwitchCheckDefault">
-                Dark Mode
-              </label>
+              <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Dark Mode</label>
             </div>
-
           </div>
         </div>
       </nav>
